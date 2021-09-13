433849655cb74f5f5f3b30dcf663ffa9eab07a82
Guillaume Dorce <43939294+polynux@users.noreply.github.com>
2021-09-13 16:25:09 +0200

    add link

--- pages/hello.js
+++ pages/hello.js
@@ -1,3 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return (<><h1>Hello 2021</h1><p>please help me</p></>)
+  return (<>
+  <h1>Hello 2021</h1>
+  <p>please help me</p>
+  <Link href="/">
+    <a>Back to home</a>
+  </Link>
+  </>)
 }
